053d2fbbe60fc7cf60ab8a779d62314239eacdf0
Ryo Takahashi <ryogiftjp@gmail.com>
2021-08-04 21:10:11 +0900

    Disabled asset logging in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.assets.quiet  = true
+  config.assets.logger = false
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
